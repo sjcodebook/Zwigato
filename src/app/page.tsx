e87aca07fb08dbf0beafae7e828b8f80157ba373
0xSJ <jainsanmati846@gmail.com>
2025-05-13 23:25:14 +0530

    feat: add login page and form components with styling updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>Mian</div>
+  return (
+    <div>
+      Mian
+      <Button>Button</Button>
+    </div>
+  )
 }
